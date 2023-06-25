137f6ae8d5cf75693bb60c968944cc1a297d1124
ayush-geek <aymalviya127@gmail.com>
2023-06-25 23:18:52 +0530

    Day 21 done

---
+++ Day 21/Kth Smallest Element in a BST.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    void solve(TreeNode *root, int &k, int &ans)
+    {
+        if (root == NULL)
+            return;
+
+        solve(root->left, k, ans);
+        k--;
+        if (k == 0)
+            ans = root->val;
+        solve(root->right, k, ans);
+    }
+
+    int kthSmallest(TreeNode *root, int k)
+    {
+        // inorder sorted order
+
+        int ans = -1;
+        solve(root, k, ans);
+        return ans;
+    }
+};
