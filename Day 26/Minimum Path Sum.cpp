d1c19377805c8bedb20caca0f6d092f0e7ce71ac
ayush-geek <aymalviya127@gmail.com>
2023-06-29 22:39:13 +0530

    Day 26

---
+++ Day 26/Minimum Path Sum.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    int minPathSum(vector<vector<int>> &grid)
+    {
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, INT_MAX));
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (i == 1 && j == 1)
+                    dp[i][j] = grid[0][0];
+                else
+                    dp[i][j] = grid[i - 1][j - 1] + min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[n][m];
+    }
+};
